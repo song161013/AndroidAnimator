a8f85718da79d02e77cfd49bf9e0bf53e67ead26
song9933 <song9933@126.com>
2021-05-06 21:57:29 +0800

    创建scale动画

--- app/src/main/java/com/animator/MainActivity.java
+++ app/src/main/java/com/animator/MainActivity.java
@@ -3,6 +3,11 @@ package com.animator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnStart(View view) {
+        ScaleAnimation scaleAnimation = (ScaleAnimation) AnimationUtils.
+                loadAnimation(this, R.anim.scale);
+
+        TextView tvStart = findViewById(R.id.tv_text);
+//        tvStart.setAnimation(scaleAnimation);
+//        scaleAnimation.start();
+
+        tvStart.startAnimation(scaleAnimation);//让动画执行是startAnimation，不是setAnimation
     }
 }
