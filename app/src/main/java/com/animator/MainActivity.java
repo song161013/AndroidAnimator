23cd1b577489211c436de05e133eb09df78592f2
song9933 <song9933@126.com>
2021-05-07 22:22:15 +0800

    添加alpha,rotate,translate,set等动画

--- app/src/main/java/com/animator/MainActivity.java
+++ app/src/main/java/com/animator/MainActivity.java
@@ -1,30 +1,56 @@
 package com.animator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvStart = findViewById(R.id.tv_text);
     }
 
-    public void btnStart(View view) {
+    public void scaleStart(View view) {
         ScaleAnimation scaleAnimation = (ScaleAnimation) AnimationUtils.
                 loadAnimation(this, R.anim.scale);
 
-        TextView tvStart = findViewById(R.id.tv_text);
 //        tvStart.setAnimation(scaleAnimation);
 //        scaleAnimation.start();
 
         tvStart.startAnimation(scaleAnimation);//让动画执行是startAnimation，不是setAnimation
+    }
+
+    public void alphaStart(View view) {
+        AlphaAnimation alphaAnimation = (AlphaAnimation) AnimationUtils.loadAnimation(this, R.anim.alpha);
+        tvStart.startAnimation(alphaAnimation);
+    }
+
+    public void roateStart(View view) {
+        RotateAnimation rotateAnimation = (RotateAnimation) AnimationUtils.loadAnimation(this, R.anim.rotate);
+        tvStart.startAnimation(rotateAnimation);
+    }
+
+    public void translateStart(View view) {
+        TranslateAnimation translateAnimation = (TranslateAnimation) AnimationUtils.loadAnimation(this, R.anim.translate);
+        tvStart.startAnimation(translateAnimation);
+    }
+
+    public void setStart(View v){
+        AnimationSet animationSet = (AnimationSet) AnimationUtils.loadAnimation(this, R.anim.set_anim);
+        tvStart.startAnimation(animationSet);
     }
 }
