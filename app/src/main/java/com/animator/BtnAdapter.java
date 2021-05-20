f6287ce61ecd6f458560ea8847465719b690ada1
song9933 <song9933@126.com>
2021-05-20 13:19:50 +0800

    interpolator 插值器

---
+++ app/src/main/java/com/animator/BtnAdapter.java
@@ -0,0 +1,86 @@
+package com.animator;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class BtnAdapter extends RecyclerView.Adapter<BtnAdapter.ViewHolde> {
+
+    private Context mContext;
+    private List<String> mDatas;
+
+    private int mViewHolderPosition;
+
+    public BtnAdapter(Context context, List<String> list) {
+        this.mContext = context;
+        this.mDatas = list;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolde onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_btn, parent, false);
+        return new ViewHolde(itemView);
+    }
+
+    public void onBindViewHolder(@NonNull ViewHolde holder, final int position) {
+        holder.markViewHolderPosition(position);
+        holder.mTvBtnId.setText(mDatas.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mItemClickListener != null) {
+                    mItemClickListener.onItemClick(position);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+    final class ViewHolde extends RecyclerView.ViewHolder {
+        private TextView mTvBtnId;
+
+        public ViewHolde(@NonNull View itemView) {
+            super(itemView);
+            mTvBtnId = itemView.findViewById(R.id.tv_btn_id);
+
+        }
+
+        public View getItemView() {
+            return itemView;
+        }
+
+
+        final void markViewHolderPosition(int pos) {
+            mViewHolderPosition = pos;
+        }
+
+        public int getViewHolderPosition() {
+            return mViewHolderPosition;
+        }
+    }
+
+    private OnItemClickListenere mItemClickListener;
+
+    public void setOnItemClickListener(OnItemClickListenere listener) {
+        this.mItemClickListener = listener;
+    }
+
+    public interface OnItemClickListenere {
+        void onItemClick(int pos);
+    }
+
+}
--- app/src/main/java/com/animator/MainActivity.java
+++ app/src/main/java/com/animator/MainActivity.java
@@ -1,20 +1,30 @@
 package com.animator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvStart;
+    RecyclerView mRvBtn;
+    private BtnAdapter mBtnAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +32,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvStart = findViewById(R.id.tv_text);
+        mRvBtn = findViewById(R.id.rv_btn);
+
+        initAdapter();
     }
 
     public void scaleStart(View view) {
-        ScaleAnimation scaleAnimation = (ScaleAnimation) AnimationUtils.
-                loadAnimation(this, R.anim.scale);
+        ScaleAnimation scaleAnimation = (ScaleAnimation) AnimationUtils.loadAnimation(this, R.anim.scale);
 
 //        tvStart.setAnimation(scaleAnimation);
 //        scaleAnimation.start();
@@ -49,8 +61,40 @@ public class MainActivity extends AppCompatActivity {
         tvStart.startAnimation(translateAnimation);
     }
 
-    public void setStart(View v){
+    public void setStart(View v) {
         AnimationSet animationSet = (AnimationSet) AnimationUtils.loadAnimation(this, R.anim.set_anim);
         tvStart.startAnimation(animationSet);
+    }
+
+    private void startInterpolator(int pos) {
+        int[] names = {R.anim.accelerate_decelerate_interpolator, R.anim.accelerate_interpolator,
+                R.anim.anticipate_interpolator, R.anim.anticipate_overshoot_interpolator, R.anim.bounce_interpolator,
+                R.anim.cycle_interpolator, R.anim.decelerate_interpolator, R.anim.linear_interpolator, R.anim.overshoot_interpolator};
+        Animation animation = AnimationUtils.loadAnimation(this, names[pos]);
+        tvStart.startAnimation(animation);
+    }
+
+    private void initAdapter() {
+        List<String> mDatas = new ArrayList<>();
+        mDatas.add("accelerate_decelerate_interpolator");
+        mDatas.add("accelerate_interpolator");
+        mDatas.add("anticipate_interpolator");
+        mDatas.add("anticipate_overshoot_interpolator");
+        mDatas.add("bounce_interpolator");
+        mDatas.add("cycle_interpolator");
+        mDatas.add("decelerate_interpolator");
+        mDatas.add("linear_interpolator");
+        mDatas.add("overshoot_interpolator");
+
+        mBtnAdapter = new BtnAdapter(this, mDatas);
+        mRvBtn.setAdapter(mBtnAdapter);
+        mRvBtn.setLayoutManager(new LinearLayoutManager(this));
+        mBtnAdapter.setOnItemClickListener(new BtnAdapter.OnItemClickListenere() {
+            @Override
+            public void onItemClick(int pos) {
+                Log.e("AABB", "pos=" + pos);
+                startInterpolator(pos);
+            }
+        });
     }
 }
